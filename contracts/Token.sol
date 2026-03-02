fe9a4217625e0176abcc559959c6f2f0a4d9f5db
Ref4te <manatoffin2006@gmail.com>
2026-03-02 09:30:12 +0500

    fix comment

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20{
     constructor() ERC20("DigitalAltyn","dAu"){
-        _mint(msg.sender, 1000*10**18); // 100 dAu
+        _mint(msg.sender, 1000*10**18); // 1000 dAu
     }
 }
