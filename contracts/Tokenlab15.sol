1e924f161e428a7650475401a5f4cd87de52bbe6
Ref4te <manatoffin2006@gmail.com>
2026-05-01 14:45:36 +0500

    some update

--- contracts/Tokenlab15.sol
+++ contracts/Tokenlab15.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.31;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20{
-    constructor() ERC20("DigitalAltyn","dAu"){
-        _mint(msg.sender, 1000*10**18); // 1000 dAu
+    constructor() ERC20("Digital Altyn V2","dAuV2"){
+        _mint(msg.sender, 10000000*10**18); // 10000000 dAuV2
     }
 }
